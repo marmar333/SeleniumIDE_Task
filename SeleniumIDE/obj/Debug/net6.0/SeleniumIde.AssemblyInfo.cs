dbf6b49cdf4f63c7f77718807fcc8a07201e9aae
MartinL <54684107+marmar333@users.noreply.github.com>
2025-02-13 12:33:58 +0200

    moved

--- SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
+++ SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0574a0ba01899b54c8374d9152f1b7fdcb81a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a84e1e0c8e283f2c6c4ea31bf98afaf88f0467")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
